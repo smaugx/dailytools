b03c7af8300a6346025d0cb2e9b6c9e327bd6913
Smaug X <linuxcode2niki@gmail.com>
2020-07-06 22:45:58 +0806

    Update flamesvg.sh

--- flamesvg.sh
+++ flamesvg.sh
@@ -30,4 +30,24 @@ if [  $1 = 'svg' ]; then
     exit 0
 fi
 
+if [  $1 = 'all' ]; then
+    record_time=10
+    if [  $2 ]; then
+        record_time=$2
+        echo "perf record time is ${record_time}"
+    fi
+
+    rm -rf perf.*
+    echo_and_run echo "sudo perf record --call-graph dwarf -a sleep ${record_time}" |bash -
+
+    echo_and_run echo "sudo perf script -i perf.data > perf.unfold" |bash -
+    echo_and_run echo "./FlameGraph/stackcollapse-perf.pl perf.unfold > perf.folded" |bash -
+    echo_and_run echo "./FlameGraph/flamegraph.pl perf.folded > perf.svg" |bash -
+
+    echo "flame svg file generated: perf.svg"
+    echo_and_run echo "ls perf.svg" |bash -
+    exit 0
+fi
+
+
 echo "error param, support ./flamesvg.sh [data|svg]"
